0bf921a46dd2f2cf6a70a9b9809b4d28376dc1aa
FairyScript <h@queb.fun>
2024-03-29 17:47:14 +0800

    fix: support farm 1.0

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,28 @@
 import type { JsPlugin } from '@farmfe/core'
 import type { TsConfigJson } from 'type-fest'
 import { parse } from 'jsonc-parser'
-import fs from 'node:fs'
+import { readFileSync } from 'node:fs'
 import path from 'node:path'
 
 export default function tsPathPlugin(): JsPlugin {
   return {
     name: 'ts-config-plugin',
-    config(param) {
-      if (!param) {
-        return param
+    config(config) {
+      if (!config) {
+        return config
       }
 
       const tsConfig: TsConfigJson = parse(
-        fs.readFileSync('./tsconfig.json', 'utf-8')
+        readFileSync(path.join(process.cwd(), './tsconfig.json'), 'utf-8')
       )
+
       const baseUrl = tsConfig.compilerOptions?.baseUrl ?? ''
-      const root = param.root ?? process.cwd()
+      const root = config.root ?? process.cwd()
+      config.root = path.resolve(root, baseUrl)
+
       const paths = tsConfig.compilerOptions?.paths ?? {}
       const trimWildcard = (str: string) => str.replace(/\/\*$/, '')
-      let alias: Record<string, string> = {}
+      const alias: Record<string, string> = {}
       for (const key in paths) {
         // farm does not support multiple path, so just use the first one
         alias[trimWildcard(key)] = path.resolve(
@@ -27,11 +30,15 @@ export default function tsPathPlugin(): JsPlugin {
           trimWildcard(paths[key][0])
         )
       }
-      alias = Object.assign(alias, param.resolve?.alias ?? {})
 
-      param.root = path.resolve(root, baseUrl)
-      param.resolve = Object.assign(param.resolve ?? {}, { alias })
-      return param
+      if (!config.compilation) config.compilation = {}
+      if (!config.compilation.resolve) config.compilation.resolve = {}
+      config.compilation.resolve.alias = Object.assign(
+        alias,
+        config.compilation.resolve.alias
+      )
+
+      return config
     },
   }
 }
